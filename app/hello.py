8c2cf88fa76a566b4d54f8cb2015002d45e3849a
pexApred <120075705+pexApred@users.noreply.github.com>
2024-09-04 17:18:43 -0400

    Added Flask-CORS extension to connect with react-native app. Added get route test data.

--- app/hello.py
+++ app/hello.py
@@ -1,6 +1,9 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
+from flask import request
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def index():
@@ -10,5 +13,13 @@ def index():
 def hello():
     return 'Hello, World'
 
+@app.get('/recipes')
+def get_data():
+    data = [
+        {'Arecipe': 'Tzatziki', 'Ingredients': 'Greek Yogurt, Cucumber, Garlic, Lemon Juice, Olive Oil, Salt, Pepper', 'Instructions': 'Grate cucumber, mix with yogurt, garlic, lemon juice, olive oil, salt, and pepper. Serve chilled.'
+        },
+    ]
+    return jsonify(data)
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
