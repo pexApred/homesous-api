e50a2e0024de9375264de4fb44fbbc115b3c125d
James Park <parkjames91@gmail.com>
2025-01-22 22:52:28 -0500

    Rename modules per naming convention

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,5 @@
-from .Freezer import Freezer
-from .Pantry import Pantry
-from .Recipe import Recipe
-from . Refrigerator import Refrigerator
-from .User import User
+from .freezer import Freezer
+from .pantry import Pantry
+from .recipe import Recipe
+from . refrigerator import Refrigerator
+from .user import User
---
+++ app/models/pantry.py
@@ -0,0 +1,2 @@
+class Pantry:
+    pass
