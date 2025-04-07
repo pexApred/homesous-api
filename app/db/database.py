11f1430847fd14ff201b07b722a95e7b61ec6b6a
pexApred <120075705+pexApred@users.noreply.github.com>
2025-04-07 10:34:16 -0400

    Refactored database class query method to include SELECT

--- app/db/database.py
+++ app/db/database.py
@@ -29,8 +29,6 @@ class Database:
                 host=self.host, 
                 port=self.port
             )
-            # Create a cursor object using the cursor() method : Default = Client Side Cursors
-            self.curs = self.conn.cursor()
             print("Connected to the database!")
         # Handle any exceptions that occur during the connection process
         except psycopg2.Error as e:
@@ -47,39 +45,14 @@ class Database:
             self.conn.close()
             print("Connection to the database is closed!")
 
-    # Dunder methods __enter__ and _-exit__ to allow the Database class to be used as a context manager(with statement) : Explicit calls to connect() and close() methods are not needed : Automatically called when the with statement is used
-    def __enter__(self):
-        self.connect()
-        return self
-    
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        if exc_type:
-            print(f"Exception happened: {exc_val}")
-        self.close()
-
     # Query method without the need to return any results : INSERT, UPDATE, DELETE : No unnecessary fetches of results
     def query(self, query, params=None):
         try:
-            # Runs the SQL query
-            self.curs.execute(query, params)
-            # Ensures changes (INSERT, UPDATE, DELETE) are committed to the database
+            with self.conn.cursor() as cur:
+                cur.execute(query, params)
+                rows = cur.fetchall() if cur.description else None
             self.conn.commit()
         except psycopg2.Error as e:
             print(f"Error executing query: {e}")
-
-    # Query to fetch results from the database : SELECT : Explicitly fetch the results
-    def fetch_query_results(self):            
-        # Fetch all the results from the cursor object as a list of tuples 
-        results = self.curs.fetchall()
-        return results if self.curs else None
-    # Can modulize further by having control over how many rows to retrieve : "def fetch_one(self):" or "def fetch_many(self, size):"
-
-# Test the Database class & Check Connection to postgres: default database prior to creating own database
-if __name__ == "__main__":
-    try: 
-        with Database() as db:
-            db.query("SELECT 1;")  # Simple query to check connection
-            result = db.curs.fetchone()
-            print(f"WORKING?: {result}")
-    except Exception as e:
-        print(f"Database connection failed: {e}")
+            rows = None
+        return rows
