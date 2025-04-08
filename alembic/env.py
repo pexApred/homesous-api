2b959d53b802fcd4e20b712a8c00a8bde93a79a6
pexApred <120075705+pexApred@users.noreply.github.com>
2025-04-08 15:58:43 -0400

    Added python-dotenv vars to sqlalchemy.url instead of exposing variables

--- alembic/env.py
+++ alembic/env.py
@@ -1,13 +1,19 @@
+import os
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-
+from dotenv import load_dotenv
+load_dotenv()
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+if DATABASE_URL:
+    config.set_main_option("sqlalchemy.url", DATABASE_URL)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
